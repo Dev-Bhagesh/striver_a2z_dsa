4e83ee081db7e6faed29229992e26524c12e6c4c
Bhagesh <bhageshniloor159@gmail.com>
2026-06-21 15:30:35 +0530

    21 june 2026

---
+++ digits.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 7789;
+    while(n>0){
+        int digit = n%10;
+        cout<<digit<<" ";
+        n = n/10;
+    }
+    return 0;
+}
