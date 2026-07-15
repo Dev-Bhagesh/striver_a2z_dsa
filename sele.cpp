5eb467a4b6f56b83e75f355bfaad62d7cd758e62
Bhagesh <bhageshniloor159@gmail.com>
2026-07-15 18:42:35 +0530

    15 jul 2026

---
+++ sele.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n = 3;
+    int min = 0;
+    int arr[n] = {1,2,0};
+    int jin = 0;
+
+    cout<<"befor sorting"<<endl;
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+     
+    cout<<endl;
+
+    for(int i = 0;i<n-1;i++){
+        min = arr[i];
+        // for(int j = i;j<=n-1;j++){
+        //     if(min> arr[j]){
+        //         // int temp = min;
+        //         arr[i] = arr[j];
+        //         arr[j] = min;
+        //     }
+        // }
+        // min = 0;
+        for(int j = i;j<=n-1;j++){
+            if(arr[j]<min){
+                min = arr[j];
+                jin = j;
+            }
+        }
+        if(jin != 0){
+            int temp = arr[i];
+            arr[i] = min;
+            arr[jin] = temp;
+        }
+        
+        jin = 0;
+
+    }
+
+    cout<<"after sorting "<<endl;
+    for(int i = 0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
